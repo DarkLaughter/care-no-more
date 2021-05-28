69179e7a9584d98103ac05ca01b688fb31696ba6
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-05-27 22:14:14 -0400

    added symbols components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
+import Symbols from './Components/Symbols/Symbols'
 
 
 const App = () => {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
-            
+            <Route exact path="/" component={Symbols} />
         </Switch>
     )
 }
---
+++ src/Components/Symbols/Symbols.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './symbols.css'
+
+const Symbols = () => {
+    return (
+        <div>
+            <h1>Marks of The Champions</h1>
+
+        </div>
+    )
+}
+
+export default Symbols
